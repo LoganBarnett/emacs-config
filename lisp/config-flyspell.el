367ff9033eaff9f4abff90b090280918ba30049f
Logan Barnett <logustus@gmail.com>
2016-12-05 17:29:12 -0800

    add flyspell custom config (use helm for the spell suggestion popup)

---
+++ lisp/config-flyspell.el
@@ -0,0 +1,25 @@
+;;; config-flyspell --- configure flyspell-mode to my liking
+;;; Commentary:
+;; flyspell mode highlights spelling errors and can provide suggested
+;; corrections
+
+;;; Code:
+
+;; configure flyspell
+(defun config-flyspell ()
+  (use-package "flyspell"
+    :ensure t
+    :init
+    (paradox-require 'flyspell-correct-helm)
+    :config
+    (define-key
+      flyspell-mode-map
+      (kbd "C-;")
+      'flyspell-correct-previous-word-generic
+      )
+    )
+    (message "set up flyspell-correct-helm - use C-; to invoke")
+  )
+
+  (provide 'config-flyspell)
+;;; config-flyspell.el ends here
