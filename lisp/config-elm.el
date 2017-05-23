c5f546b90227344b28b8f8b6b58159491da6d7c1
Logan Barnett <logustus@gmail.com>
2017-05-23 10:51:46 -0600

    add elm utilities

---
+++ lisp/config-elm.el
@@ -0,0 +1,17 @@
+;;; config-elm --- configure elm support
+;;; Commentary:
+;; the layer sets up a lot of this
+
+;;; Code:
+
+;; configure elm
+(defun config-elm ()
+  ;; (use-package "elm"
+    ;; :init
+    ;; :config
+    (setq-default elm-indent-offset 2)
+    ;; )
+  )
+(provide 'config-elm)
+
+;;; config-elm.el ends here
