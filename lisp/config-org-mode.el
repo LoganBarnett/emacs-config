89ce49e1bc32e948e167c115083c522de77b99a2
Logan Barnett <logustus@gmail.com>
2017-05-28 09:39:16 -0600

    move org-mode configuration to a separate file

---
+++ lisp/config-org-mode.el
@@ -0,0 +1,58 @@
+;;; config-org-mode --- configure org support
+;;; Commentary:
+;; Configure org-mode to my liking.
+;;; Code:
+(require 'use-package)
+(require 'org)
+
+;; TODO: Setup a keybinding to replace org-clock-report with this function.
+(defun my/org-clock-report ()
+  "Run org-clock-report but don't leave a narrowed buffer when done."
+  (interactive)
+  (org-clock-report)
+  (widen))
+
+;; configure org-mode
+(defun config-org-mode ()
+  "Configure 'org-mode'."
+  (use-package "org-mode"
+    ;; :init
+    :config
+    ;; set default diary location
+    (setq-default diary-file "~/notes/diary.org")
+    ;; (setq-default appt-audible t)
+    (setq-default calendar-date-style 'iso)
+    (setq-default org-agenda-files '("~/notes/planner.org"))
+    ;; shrink inline images see:
+    ;; http://lists.gnu.org/archive/html/emacs-orgmode/2012-08/msg01388.html
+    (setq-default org-src-fontify-natively t)
+    (setq-default org-image-actual-width '(400))
+    (add-hook 'org-mode-hook 'auto-fill-mode)
+    ;; Use my custom org clock report function, which prevents narrowing. I find
+    ;; narrowing during this operation confusing.
+    ;; (add-hook 'org-mode-hook (lambda ()
+    ;;                            (bind-key "C-c C-x C-r" 'my/org-clock-report)
+    ;;                            ))
+    (global-set-key (kbd "C-c C-x C-r") 'my/org-clock-report)
+    ;; For some reason this doesn't work. How do I override key bindings?
+    (bind-key (kbd "C-c C-x C-r") 'my/org-clock-report)
+    ;; Some initial langauges we want org-babel to support
+    (require 'ob-js)
+    (org-babel-do-load-languages
+     'org-babel-load-languages
+     '(
+       (sh . t)
+       (python . t)
+       (R . t)
+       (ruby . t)
+       (ditaa . t)
+       (dot . t)
+       (octave . t)
+       (sqlite . t)
+       (perl . t)
+       (js . t)
+       ))
+    ))
+(provide 'config-org-mode)
+
+;;; config-org-mode.el ends here
