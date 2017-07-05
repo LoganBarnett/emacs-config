baa020c529307188d68d31dd9140966ffe8e253e
Logan Barnett <logustus@gmail.com>
2017-07-04 23:02:29 -0700

    extract money helpers to another elisp file

---
+++ lisp/money.el
@@ -0,0 +1,34 @@
+;;; money -- monetary calculations
+;;; Commentary:
+;; A module relating to monetary calculations.
+;;; Code:
+
+;; for reference - this was a port of the JS version (it might be incomplete
+;; port-wise) on this site:
+;; https://home.ubalt.edu/ntsbarsh/Business-stat/otherapplets/CompoundCal.htm#rjava9
+;; in the "Accelerating Mortgage Payments" form.
+;; (defun my/debt-months-remaining2 (principle monthly-payment interest-rate)
+;;   (/
+;;    (log (/ monthly-payment (- monthly-payment (* principle interest-rate))))
+;;    (log (+ 1 interest-rate))
+;;    )
+;;   )
+(defun my/debt-months-remaining (principle monthly-payment interest-rate)
+  "Calculates the months (payments) remaining on a PRINCIPLE given a MONTHLY-PAYMENT with an INTEREST-RATE."
+  (- (log
+      (- 1
+         (/ (* principle interest-rate) monthly-payment)
+         )
+      (+ 1 interest-rate)
+      )))
+
+(defun my/debt-months-remaining-from-org-table (principle
+                                                monthly-payment
+                                                interest-rate)
+  "(Assumes an org table for inputs) Calculates the months (payments) remaining on a PRINCIPLE given a MONTHLY-PAYMENT with an INTEREST-RATE."
+  (my/debt-months-remaining (string-to-number monthly-payment)
+                            (string-to-number monthly-payment)
+                            (string-to-number interest-rate))
+  )
+
+;;; money.el ends here
