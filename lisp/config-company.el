dfaa751b282a1f8bad545c48f94d651e92cf78d7
Logan Barnett <logustus@gmail.com>
2017-11-23 05:21:49 -0800

    fix the company-flow backend never bringing up completions

--- lisp/config-company.el
+++ lisp/config-company.el
@@ -6,7 +6,6 @@
 (defun config-company ()
   "Run the company configuration."
   (defvar company-active-map)
-  (defvar company-flow-modes)
   (declare-function paradox-require "ext:paradox-require")
   (declare-function global-company-mode "ext:global-company-mode")
   (declare-function evil-declare-change-repeat "ext:evil-declare-change-repeat")
@@ -34,43 +33,6 @@
     (define-key company-active-map [tab] 'company-complete-selection)
     (define-key company-active-map (kbd "C-n") 'company-select-next)
     (define-key company-active-map (kbd "C-p") 'company-select-previous)
-
-    ;; backends
-    (paradox-require 'company-flow)
-    (add-hook 'company-mode-hook
-              (apply-partially #'my/use-bin-from-node-modules "flow"))
-    ;; (spacemacs|defvar-company-backends javascript-mode)
-    ;; (add-to-list 'company-backends-javascript-mode 'company-flow)
-    ;; (add-to-list 'company-backends-js2-mode 'company-flow)
-    )
-  )
-
-;; Commentary: add node_modules bin files for the project we are in
-;; TODO: there is a very similar custom function for flychecker here
-;; see about consolidating the two
-;;; Code:
-(defun my/use-bin-from-node-modules (bin-name)
-  "Add node_modules bin files for project we are currently in.
-BIN-NAME: the name of the binary to use"
-  ;; (message "company -- setting %s exec for mode %s" bin-name major-mode )
-  (defvar company-flow-executable)
-  (let ((path "invalid")))
-  (let* ((root (locate-dominating-file
-                (or (buffer-file-name) default-directory)
-                "node_modules"))
-         (path (and root
-                    (expand-file-name (concat "node_modules/.bin/" bin-name)
-                                      root))))
-    (if path
-        ;; (when (file-executable-p path)
-        (let ((exec-sym (intern (concat "company-" bin-name "-executable"))))
-          (make-local-variable exec-sym)
-          (set exec-sym path)
-          ;; (message "company -- exec is %s" company-flow-executable)
-          )
-      ;; (message "company -- backend %s not available for mode %s with file %s"
-               ;; bin-name major-mode buffer-file-name)
-      )
     )
   )
 
