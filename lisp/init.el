1f1d9f34da87e5079789639c48e57cf562c50a3a
Logan Barnett <logustus@gmail.com>
2021-05-17 19:09:17 -0700

    multi-line trailing commas for Rust
    Also cleans usage of multi-line (mostly JavaScript) and should speed up
    initial load of Emacs a little bit since requires should be deferred
    more now.

--- lisp/init.el
+++ lisp/init.el
@@ -110,23 +110,6 @@
   (setq-default tab-width 2)
 
   ;; prog-mode stuff
-  ;; multi-line
-  ;; always add new line rather than flowing
-  (require 'multi-line)
-  (defvar multi-line-always-newline)
-  (defvar multi-line-current-strategy)
-  (setq-default multi-line-current-strategy
-                (multi-line-strategy
-                 :respace (multi-line-default-respacers
-                           (make-instance multi-line-always-newline))))
-  ;; (multi-line-defhook js2
-  ;;   (make-instance multi-line-strategy
-  ;;                  :find (make-instance 'multi-line-forward-sexp-find-strategy
-  ;;                                       :split-regex "[[:space:]\n]+"
-  ;;                                       :done-regex "[[:space:]]*)]}"
-  ;;                                       :split-advance-fn 'js2-mode-forward-sexp)
-  ;;                  :respace multi-line-always-newline-respacer))
-    (multi-line-defhook js2 multi-line-add-trailing-comma-strategy t)
   ;; (use-package "color-identifiers-mode"
   ;;   :ensure t
   ;;   :init
