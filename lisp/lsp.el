d14ef6ddbc79364bac8bf6d5c9d3875c0a1d6b5d
Logan Barnett <logustus@gmail.com>
2025-08-02 17:08:26 -0700

    show docs, and show them in the minibuffer

--- lisp/lsp.el
+++ lisp/lsp.el
@@ -6,6 +6,12 @@
 (use-package "lsp-mode"
   :init
   :config
+  ;; Show us more complete and helpful information under the cursor.
+  (setq lsp-ui-doc-enable nil)
+  (setq lsp-ui-doc-show-with-cursor nil)
+  (setq lsp-eldoc-render-all t)          ;; show all info, not truncated
+  (setq lsp-idle-delay 0.2)              ;; faster response
+  (setq lsp-eldoc-enable-hover t)        ;; enable hover in minibuffer
   (map!
    :mode lsp-mode
    :localleader
