a105e5f29d17b2bb5461d1320e649bf9e2a6bf2b
Logan Barnett <logustus@gmail.com>
2025-11-17 17:26:36 -0800

    fix plantuml to work with a Nix provided plantuml

--- lisp/config-plantuml.el
+++ lisp/config-plantuml.el
@@ -9,15 +9,25 @@
 ;; configure plantuml
 (defun config-plantuml ()
   "Configure Plantuml."
+  (interactive)
+
   (use-package "plantuml-mode"
-  ;; :init
-  :config
-  (setq-default plantuml-jar-path
-                "/usr/local/opt/plantuml/libexec/plantuml.jar")
-  (setq-default org-plantuml-jar-path
-                "/usr/local/opt/plantuml/libexec/plantuml.jar")
-  (setq-default plantuml-java-args "java.awt.headless=true")
-  ))
+    :config
+    (setq-default plantuml-exec-mode 'executable)
+    ;; (setq-default plantuml-jar-path
+    ;;               "/usr/local/opt/plantuml/libexec/plantuml.jar")
+    ;; (setq-default org-plantuml-jar-path
+    ;;               "/usr/local/opt/plantuml/libexec/plantuml.jar")
+    (setq-default plantuml-java-args "java.awt.headless=true")
+    )
+
+  (use-package ob-plantuml
+    :config
+    (setq-default org-plantuml-exec-mode 'plantuml)
+    )
+
+  )
+
 (provide 'config-plantuml)
 
 ;;; config-plantuml.el ends here
